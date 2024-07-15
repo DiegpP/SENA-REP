59e220b0f2f0e53898b6181a4325e245bd3e07f5
DiegopP <darktro900@gmail.com>
2024-07-14 21:40:46 -0500

    Registrase

---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,52 @@
+import { connectDB } from "@/libs/mongodb";
+import { NextResponse } from 'next/server';
+import User from "@/models/user";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: Request) {
+    const { fullname, email, password } = await request.json();
+    console.log(fullname, email, password);
+
+    if (!password || password.lenght < 6)
+        return NextResponse.json(
+          { 
+            message: "Password must be at least 6 characters" 
+        },
+          { 
+            status: 400, }
+        );
+
+
+    try {
+        await connectDB();
+    const userFound = await User.findOne({ email });
+
+    if (userFound) return NextResponse.json({
+            message: "Email already exists",
+          },
+          {
+            status: 409,
+          }
+        );
+
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+        const user = new User({
+            email,
+            fullname,
+            password: hashedPassword,
+          });
+        
+        const savedUser = await user.save();
+        console.log(savedUser);
+
+        return NextResponse.json(savedUser);
+    } catch (error){
+        console.log(error);
+        return NextResponse.error();
+    }
+      
+          
+    return NextResponse.json({message : "signup"})
+}
