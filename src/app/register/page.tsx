21dcfdb5dfd9442c85b9260cb65c68c7ebcb039b
DiegopP <darktro900@gmail.com>
2024-07-15 20:58:07 -0500

    Forma del registro

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+"use client"
+import { FormEvent, useState } from 'react';
+import axios, { AxiosError } from 'axios';
 
-export default function RegisterPage() {
+function RegisterPage() {
+  
+  const[error, setError] = useState();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget);
+    try{
+      const res = await axios.post("/api/auth/signup",{  
+        email: formData.get("email"),
+        password: formData.get("password"),
+        fullname: formData.get("fullname")
+      });
+      console.log(res);
+    }catch(error){
+      console.log(error)
+      if (error instanceof AxiosError){
+        setError(error.response?.data.message);
+      }
+    }   
+  };
   return (
-    <div>RegisterPage</div>
-  )
+    <div>
+      <form onSubmit={handleSubmit}>
+        {error && <div className='bg-red-500 text-white p-2 mb-2'>{error}
+          </div>}
+        <h1 >Signup</h1>
+
+        <input
+          type="text"
+          placeholder="Fullname"
+          className="bg-zinc-800 px-4 py-2 block mb-2 "
+          name="fullname"
+        />
+
+        
+        <input
+          type="email"
+          placeholder="Email"
+          className="bg-zinc-800 px-4 py-2 block mb-2 "
+          name="email"
+        />
+
+      
+        <input
+          type="password"
+          placeholder="Password"
+          className="bg-zinc-800 px-4 py-2 block mb-2 "
+          name="password"
+        />
+
+        <button className="bg-blue-500 text-white px-4 py-2 block mt-4">
+          Registrarse
+        </button>
+        
+
+      </form>     
+    </div>
+  );
 }
+export default RegisterPage;
 
 
