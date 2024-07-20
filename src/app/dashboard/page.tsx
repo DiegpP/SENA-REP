c20477449469d0bb3c6586855da6e959326b6b53
DiegopP <darktro900@gmail.com>
2024-07-20 11:16:41 -0500

    Configuracion del NexthAuth

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
+
+function DashboardPage() {
+  const { data: session, status } = useSession();
+
+  console.log(session, status);
+  return <div>DashboardPage</div>;
+}
+
+export default DashboardPage;
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,22 +1,36 @@
 "use client"
 import { FormEvent, useState } from 'react';
 import axios, { AxiosError } from 'axios';
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 function RegisterPage() {
   
   const[error, setError] = useState();
+  const router = useRouter()
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
     const formData = new FormData(e.currentTarget);
+    
     try{
-      const res = await axios.post("/api/auth/signup",{  
+      const signupResponse = await axios.post("/api/auth/signup",{  
         email: formData.get("email"),
         password: formData.get("password"),
         fullname: formData.get("fullname")
       });
-      console.log(res);
+      console.log(signupResponse);
+
+      const res = await signIn("credentials", {
+        email: signupResponse.data.email,
+        password: formData.get("password"),
+        redirect: false,
+      });
+
+      if (res?.ok) return router.push("/dashboard");
+    
+    console.log(res);
     }catch(error){
       console.log(error)
       if (error instanceof AxiosError){
@@ -24,6 +38,8 @@ function RegisterPage() {
       }
     }   
   };
+  
+  
   return (
     <div>
       <form onSubmit={handleSubmit}>
